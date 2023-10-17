e5fa4451e66e90fdd9ee9ba81776c9c789a71cad
gustavo antonio <143827972+gustavoantonio7@users.noreply.github.com>
2023-10-17 11:21:59 -0300

    att

---
+++ aula01/ex03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
+$color = "red";
+echo "My car is." . $color . "<br>";
+echo "My house is." . $color . "<br>";
+echo "My boat is." . $color. "<br>";
+
+
+?>
