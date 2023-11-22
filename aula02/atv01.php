4b987347002bb0b5cfc354351d85c2836883895e
gustavo antonio <143827972+gustavoantonio7@users.noreply.github.com>
2023-11-22 18:42:11 -0300

    att no repositorio

--- aula02/atv01.php
+++ aula02/atv01.php
@@ -2,21 +2,21 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>atv01</title>
-  <link rel="stylesheet" href="atv01.css">
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>atv01</title>
+     <link rel="stylesheet" href="atv01.css">
 </head>
 
 <body>
 
-  <div class="p1">
+     <div class="p1">
 
-    <div class="p4">TEXTO</div>
+          <div class="p4">TEXTO</div>
 
-  </div>
-  <div class="p2"></div>
-  <div class="p3"></div>
+     </div>
+     <div class="p2"></div>
+     <div class="p3"></div>
 </body>
 
 </html>
